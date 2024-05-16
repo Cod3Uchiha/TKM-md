9523ad3ff5272cd5a6ca213fdb6d9373f899af7a
Cod3Uchiha <157640942+Cod3Uchiha@users.noreply.github.com>
2024-05-16 23:14:19 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+#currently being redeveloped BY Cod3uchiha
+
+apt-get update
+apt-get upgrade
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
